4a85b51a47efa18498e7c1ad0204253255f55d8d
giuseppe <Giuseppearimatea10@gmail.com>
2020-03-16 18:45:10 +0100

    fatto

--- resources/views/cds.blade.php
+++ resources/views/cds.blade.php
@@ -1,3 +1,4 @@
+{{-- @dd($data); --}}
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,8 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-    <title>Php Dischi</title>
+    <title>cd</title>
 </head>
 
 <body>
@@ -16,50 +16,19 @@
         </header>
 
         <div class="cds-container">
+            @foreach ($data as $cd)
             <div class="cd">
-                <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" />
+                <img src="{{$cd['poster']}}" />
 
-                <h3>New Jersey</h3>
-                <small>Bon Jovi</small>
-                <strong>1988</strong>
+                <h4>{{$cd['title']}}</h4>
+                <small>{{$cd['author']}}</small>
+                <small>{{$cd['year']}}</small>
             </div>
-            <div class="cd">
-                <img src="https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg" />
-
-                <h3>Live at Wembley 86</h3>
-                <small>Queen</small>
-                <strong>1992</strong>
-            </div>
-            <div class="cd">
-                <img src="https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg" />
-
-                <h3>Ten's Summoner's Tales</h3>
-                <small>Sting</small>
-                <strong>1993</strong>
-            </div>
-            <div class="cd">
-                <img src="https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg" />
-
-                <h3>Steve Gadd band</h3>
-                <small>Steve Gadd Band</small>
-                <strong>2018</strong>
-            </div>
-            <div class="cd">
-                <img src="https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg" />
-
-                <h3>Brave new World</h3>
-                <small>Iron Maiden</small>
-                <strong>2000</strong>
-            </div>
-            <div class="cd">
-                <img src="https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg" />
-
-                <h3>One more car, one more rider</h3>
-                <small>Eric Clapton</small>
-                <strong>2002</strong>
-            </div>
+            @endforeach
         </div>
     </div>
 </body>
+
+</html>
 
 </html>
